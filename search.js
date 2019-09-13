b91772d3735786c009055de04582ce42e74a8c09
onluiz <luiz.alberto1723@gmail.com>
2019-09-13 14:16:36 -0300

    Add endpoint to query a movie by its name attr

---
+++ search.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  try {
+    const { query, page } = event.queryStringParameters;
+    const movies = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.tmdbSecretKey}&language=en-US&query=${query}&page=${page}&include_adult=false`);
+    return success(movies.data);
+  } catch (e) {
+    return failure({ status: false, error: e, });
+  }
+}
